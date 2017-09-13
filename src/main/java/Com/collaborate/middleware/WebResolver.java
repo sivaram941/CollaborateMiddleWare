1b153744eea489857483bdac20f5825a4441ae1e
sivaram941 <sivaram941@gmail.com>
2017-09-13 21:01:09 +0530

    WebReslover Added

--- src/main/java/Com/collaborate/middleware/WebResolver.java
+++ src/main/java/Com/collaborate/middleware/WebResolver.java
@@ -1,13 +1,22 @@
 package Com.collaborate.middleware;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan
+@ComponentScan("Com.collaborate.collabaration")
 public class WebResolver {
 	
-
+	@Bean
+	public InternalResourceViewReslover.getViewResolver(){
+		InternalResourceViewResolver.internalResourceViewResolver = new InternalResourceViewResolver();
+		InternalResourceViewResolver.setPrefix("/WEB-INF");
+		InternalResourceViewResolver.setSuffix(".jsp");
+		System.out.println("Resolver Created");
+		return InternalResourceViewReslover;
+	}
 }
